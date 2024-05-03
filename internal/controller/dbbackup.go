09842796e209bbe892f3501f491d875a5e126cd6
code-horse-mi <945623397@qq.com>
2024-05-03 17:56:43 +0800

    完善备份任务

--- internal/controller/dbbackup.go
+++ internal/controller/dbbackup.go
@@ -13,14 +13,13 @@ import (
 func (r *DbManageReconciler) DbBackupTask(dbManage *operatorcodehorsecomv1beta1.DbManage) error {
 	dbBackupDir := "/tmp/dbbackup"
 	_, err := os.Stat(dbBackupDir)
-	if err == nil {
-		return fmt.Errorf("%s文件夹已经创建", dbBackupDir)
-	}
-	if errx := os.MkdirAll(dbBackupDir, 0700); errx == nil {
-		operatorcodehorsecomv1beta1.L().Info().Msgf("%s文件夹创建成功", dbBackupDir)
-	} else {
-		operatorcodehorsecomv1beta1.L().Info().Msgf("%s文件夹创建失败, 原因: %s", dbBackupDir, errx.Error())
-		return errx
+	if err != nil {
+		if errx := os.MkdirAll(dbBackupDir, 0700); errx == nil {
+			operatorcodehorsecomv1beta1.L().Info().Msgf("%s文件夹创建成功", dbBackupDir)
+		} else {
+			operatorcodehorsecomv1beta1.L().Info().Msgf("%s文件夹创建失败, 原因: %s", dbBackupDir, errx.Error())
+			return errx
+		}
 	}
 	backupCmd := fmt.Sprintf("mysqldump -u%s -p%s -h%s -P%d --all-databases > /tmp/dbbackup/%s.sql",
 		dbManage.Spec.Origin.Username,
@@ -28,7 +27,7 @@ func (r *DbManageReconciler) DbBackupTask(dbManage *operatorcodehorsecomv1beta1.
 		dbManage.Spec.Origin.Host,
 		dbManage.Spec.Origin.Port,
 		time.Now().Format("0102150405"))
-	_, err = exec.Command(backupCmd).Output()
+	_, err = exec.Command("bash", "-c", backupCmd).Output()
 	if err != nil {
 		return err
 	}
